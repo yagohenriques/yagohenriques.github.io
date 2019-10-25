8ddaacd0d774d58e22fbb77186477b2a82695d05
yagohenriques <49690675+yagohenriques@users.noreply.github.com>
2019-10-25 15:42:41 -0300

    Add files via upload

---
+++ freeway/carregarArquivos.js
@@ -0,0 +1,35 @@
+//Variáveis para as imagens
+let imgEstrada;
+let imgVaca;
+let imgCarro = [];
+
+function preload(){
+  imgEstrada = loadImage('imagens/estrada.png');
+  imgVaca = loadImage('imagens/ator-1.png');
+  imgTouro = loadImage('imagens/ferdinando.png');
+  
+  imgCarro[0] = loadImage('imagens/carro-1.png');
+  imgCarro[1] = loadImage('imagens/carro-2.png');
+  imgCarro[2] = loadImage('imagens/carro-3.png');
+  
+  imgCarro[3] = loadImage('imagens/carro-4.png');
+  imgCarro[4] = loadImage('imagens/carro-5.png');
+  imgCarro[5] = loadImage('imagens/carro-6.png');
+  
+  ponto = loadSound('sons/pontos.wav');
+  atropelou = loadSound('sons/colidiu.mp3');
+  trilha = loadSound('sons/trilha.mp3');
+}
+
+//Variáveis de movimento
+let yVaca = 366;
+let yTouro = 366;
+
+let xVaca = 150;
+let xTouro = 450;
+
+//Vetores
+//            Direita        /  Esquerda
+let xCarro = [615, 605, 610, -60, -65, -70];
+let yCarro = [45, 105, 158, 215, 270, 325];
+let velocidadeCarro = [2, 3, 4, 3, 5, 2];
---
+++ freeway/sketch.js
@@ -0,0 +1,70 @@
+function setup() {
+ createCanvas(600, 400);
+}
+function setup(){
+createCanvas(600, 400);
+  trilha.setVolume(0.2);
+  //trilha.loop();
+}
+
+function draw() {
+ if(frameCount > 400){// + ou -* 8 segundos//
+   if(PlacarVaca < 5 && PlacarTouro < 5)
+   jogar();
+   else
+     verificaVencedor();
+ }else{
+   //TelaInicial
+   telaInicial();
+ }
+}
+
+function verificaVencedor(){
+ if(PlacarVaca >= 5)
+   vacaVencedora();
+ else
+   touroVencedor();
+}
+
+function vacaVencedora(){
+ fill("Crimson");
+ textStyle(BOLD);
+ textAlign(CENTER);
+ fill("Chartreuse");
+ textSize(40);
+ text("Vaca Venceu", 150, 90);
+ image(imgVaca, 200, 100, 200, 200);
+}
+
+
+function touroVencedor(){
+ fill("DarkOrchid");
+ textStyle(BOLD);
+ textAlign(CENTER);
+ fill("DarkOrange");
+ textSize(40);
+ text("Touro Venceu", 150, 90);
+ image(imgTouro, 200, 100, 200, 200);
+}
+
+function telaInicial(){
+ fill("Chartreuse");
+ background("silver");
+ textStyle(BOLD);
+ textAlign(CENTER);
+ fill("Chocolate");
+ textSize(40);
+ text("Jogo da Av. 1º de Maio", 300, 200);
+ textSize(25);
+ text("Desenvolvido por Patric", 300, 230);
+}
+
+function jogar(){
+ background(imgEstrada);
+ mostraCarros();
+ mostraVaca();
+ movimentaCarros();
+ movimentaVaca();
+ atropelamento();
+mostraPlacar();
+}
